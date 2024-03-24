22241a3fd258da88e77c500d4adafb41d08814ae
hamutan-86 <158189349+hamutan-86@users.noreply.github.com>
2024-03-24 12:10:56 +0900

    Create main.py

---
+++ main.py
@@ -0,0 +1,51 @@
+from colorama import Fore,init
+import threading
+import secrets
+import random
+import requests
+import time
+import toml
+import os
+
+init(autoreset=True)
+config = toml.load("./config.toml")
+
+if not os.path.isfile(config["proxies"]):
+  print(f"{Fore.RED}[!] proxies are wrong. set to txt file path or blank")
+  while True:
+    pass
+if config["proxies"] == "":
+  proxies = ["None"]
+else:
+  with open(config["proxies"], "r") as f:
+    s = f.read()
+    proxies = s.split("\n")
+
+def gen():
+  global proxies
+  while True:
+    k = secrets.token_hex(100)[:32].upper()
+    key = f"CAP-{k}"
+    if "None" in proxies:
+      res = requests.post("https://api.capsolver.com/getBalance", json={"clientKey": key})
+    else:
+      proxy = random.choice(proxies)
+      res = requests.post("https://api.capsolver.com/getBalance", json={"clientKey": key}, proxies={"https": f"http://{proxy}"})
+    if res.status_code in range(200, 299):
+      balance = res.json()["balance"]
+      print(f"{Fore.GREEN}[+] Working Key: {key} | Balance: {balance}")
+      with open("capmon.txt", "w") as f:
+        f.write(key)
+    elif "account authorization is invalid" in res.text:
+      print(f"{Fore.RED}[!] Not Working Key: {key}")
+    else:
+      print(f"{Fore.RED}[!] Error: {key} | {res.status_code} | {res.text}")
+    time.sleep(config["interval"])
+
+threads = []       
+for f in range(config["threads"]):
+  threads.append(threading.Thread(target=gen))
+for thread in threads:
+  thread.start()
+for thread in threads:
+  thread.join()
